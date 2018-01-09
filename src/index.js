411b865de56f7f9361234c7ca4b175bfa5f7a8ec
[Hidekazu] <itsyuka101@gmail.com>
2018-01-09 10:42:50 -0600

    Add in more definition files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const Beatmap = require('./Beatmap')
 const Colour = require('./Colour')
+const Mods = require('./Enum/Mods')
+const HitSound = require('./Enum/HitSound')
+const HitType = require('./Enum/HitType')
 
 module.exports = Beatmap
 module.exports.Colour = Colour
+module.exports.Mods = Mods
+module.exports.HitSound = HitSound
+module.exports.HitType = HitType
